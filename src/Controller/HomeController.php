bd1188764c99178484bd17d7b35cb4619121c459
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2025-06-18 10:53:23 +0200

    Cours 2

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+        return $this->render('base.html.twig', [
+            'controller_name' => 'HomeController',
+            'number' => $number,
+        ]);
+    }
+}
