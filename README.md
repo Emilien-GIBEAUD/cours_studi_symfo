742a9e60cec260f75a5c81928be5d1a7026d902a
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2025-06-20 15:12:29 +0200

    Cours 11  validation (voir README.md)

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ C'est pas du symfony ...
 
 Cours 10 :
 Le profiler est installé avec la webapp (ou bien c'est avec la v7)
+
+
+Cours 11 : 
+à éclaircir au moment venu...
+utiliser les attributes dans l'entity puis "$form->isSubmitted() && $form->isValid()" dans le controlleur
+peut-être à adapter pour l'api.
---
+++ src/Form/TaskForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Dom\Text;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class TaskForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous devez saisir un titre.',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Le titre doit contenir au moin {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('content',TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
