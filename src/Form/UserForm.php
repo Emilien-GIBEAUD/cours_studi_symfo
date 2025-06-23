7e34f732a260099c36ae546dfe436c0a28264faf
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2025-06-23 10:17:36 +0200

    Cours 12 sécu et user

--- src/Form/UserForm.php
+++ src/Form/UserForm.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +14,9 @@ class UserForm extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('password',PasswordType::class)
             ->add('firstName')
             ->add('lastName')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
-            ])
             ->add('pseudo')
         ;
     }
